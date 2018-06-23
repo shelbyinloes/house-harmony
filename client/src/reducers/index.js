55b1d7cbf9c2335e2ecdb8fcc91ed5a404ff1079
Shelby Inloes <shelbyinloes@gmail.com>
2018-06-23 12:01:44 -0500

    reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,11 +1,21 @@
-import {combineReducers} from "redux";
+import { combineReducers } from "redux";
 
-function someThing(state = "") { 
+function dailyChores(state = [], action) { 
+  if (action.type === "DAILY_CHORES_FETCHED") {
+    return action.value;
+  }
+  return state;
+}
+
+function weeklyChores(state = [], action) { 
+  if (action.type === "WEEKLY_CHORES_FETCHED") {
+    return action.value;
+  }
   return state;
 }
 
 
 const rootReducer = combineReducers({
-  someThing
+  dailyChores, weeklyChores
 });
 export default rootReducer;
