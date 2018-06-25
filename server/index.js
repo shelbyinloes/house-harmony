14f558653ef88598bdeff1078c2109c51d5608bf
Shelby Inloes <shelbyinloes@gmail.com>
2018-06-24 21:47:23 -0500

    app.use + Routes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
 mongoose.set("debug", true);
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/checkpoint2");
+mongoose.connect("mongodb+srv://hharmony:acafinal@final-project-gaat4.mongodb.net/test?retryWrites=true");
 
+const dailyChoreRoutes = require("./routes/dailyChoreRoutes");
+const weeklyChoreRoutes = require("./routes/weeklyChoreRoutes");
+
+app.use(dailyChoreRoutes);
+app.use(weeklyChoreRoutes);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
