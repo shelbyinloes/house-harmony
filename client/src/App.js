7c3ea70074ab7eb409780e009a8a5866109b4c5a
Shelby Inloes <shelbyinloes@gmail.com>
2018-06-24 21:46:52 -0500

    import react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { BrowswerRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import DailyChores from "./containers/DailyChoresContainer";
