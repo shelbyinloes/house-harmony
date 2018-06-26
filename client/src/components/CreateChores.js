a7ccc6e2d53524ad146a57ed4945764a6a5e22c2
Shelby Inloes <shelbyinloes@gmail.com>
2018-06-26 13:27:18 -0500

    create file

---
+++ client/src/components/CreateChores.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+class CreateChores extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      dailyChore: {
+        chore: ""
+      },
+      weeklyChore: {
+        chore: ""
+      }
+    };
+  }
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Daily Chores</h1>
+          <form onSubmit={(e) => {
+            e.preventDefault();
+            if (this.props.createDailyChore) {
+              this.props.createDailyChore(this.state.dailyChore);
+            }
+          }}>
+            <div>
+              Chore: <input onChange={(e) => {
+                const dailyChore = {body: e.target.value};
+                this.setState({
+                  chore: Object.assign(this.state.dailyChore,dailyChore)
+                });
+              }} />
+            </div>
+            <button>Create</button> 
+          </form>
+        </div>
+        <div>
+          <h1>Weekly Chores</h1>
+          <form onSubmit={(e) => {
+            e.preventDefault();
+            if (this.props.createWeeklyChore) {
+              this.props.createWeeklyChore(this.state.weeklyChore);
+            }
+          }}>
+            <div>
+              Chore: <input onChange={(e) => {
+                const weeklyChore = {body: e.target.value};
+                this.setState({
+                  chore: Object.assign(this.state.weeklyChore,weeklyChore)
+                });
+              }} />
+            </div>
+            <button>Create</button> 
+          </form>
+        </div>
+      </div>
+      
+    );
+  }
+}
+export default CreateChores;
