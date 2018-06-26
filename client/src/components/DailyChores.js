d1f79af1bdefd507ba866742a28c2eea60d18297
Shelby Inloes <shelbyinloes@gmail.com>
2018-06-26 13:28:43 -0500

    update

--- client/src/components/DailyChores.js
+++ client/src/components/DailyChores.js
@@ -1,24 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 // import { connect } from "react-redux";
 
-function DailyChores(props) {
-  const dashDiv = {
-    padding: "10px",
-    backgroundColor: "blue",
-    borderRadius: "5px"
-  };
+class DailyChores extends Component {
+  componentDidMount() {
+    const id = this.props.match.params.id;
+    this.props.loadDailyChore(id);
+  }
+  
+  render() {
+    const dashDiv = {
+      padding: "10px",
+      backgroundColor: "blue",
+      borderRadius: "5px"
+    };
       
-  return ( 
-    <div style={dashDiv} className="col-sm-6">
-      <h3>Daily Chores</h3>
-      <ul>
-        {props.dailyChores.map((chore, i) => {
-          return <li key={i}>{chore}</li>;
-        })}
-      </ul>
+    return ( 
+      <div style={dashDiv} className="col-sm-6">
+        <h3>Daily Chores</h3>
+        <li>{this.props.dailyChores.chore}</li>
       </div>
-  );
+    );
+  }
 }
 
 // function mapStateToProps(state) {
