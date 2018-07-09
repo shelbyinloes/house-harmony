a2ce79d892d9a4a725dc67d89e7ef5123f30557a
Shelby Inloes <shelbyinloes@gmail.com>
2018-07-08 20:27:06 -0500

    tabs

--- client/src/components/Admin.js
+++ client/src/components/Admin.js
@@ -1,27 +1,41 @@
-import React from "react";
+import React, {Component} from "react";
 import CollapsableMapper from "./CollapsableMapper";
 import CreateChoresContainer from "../containers/CreateChoresContainer";
 import {Tabs, Tab} from "react-bootstrap";
 
-function DailyChores(props) {
-  return (
-    <div className="col-sm-6">
-      <h2>Daily Chore Admin</h2>
-      <CollapsableMapper data={props.dailyChores} path="dailyChore" field="chore" />
-      <CollapsableMapper data={props.weeklyChores} path="weeklyChore" field="chore" />
-      <CreateChoresContainer />
-      <Tabs defaultActiveKey={2} id="uncontrolled-tab-example">
-        <Tab eventKey={1} title="Tab 1">
-          Tab 1 content
-        </Tab>
-        <Tab eventKey={2} title="Tab 2">
-          Tab 2 content
-        </Tab>
-        <Tab eventKey={3} title="Tab 3" disabled>
-          Tab 3 content
-        </Tab>
-      </Tabs>
-    </div>
-  );
+class Admin extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dailyChore: {
+        chore: ""
+      },
+      weeklyChore: {
+        chore: ""
+      }
+    };
+  }
+  render() {
+    return (
+      <div className="col-sm-6">
+        <h2>Daily Chore Admin</h2>
+        <CreateChoresContainer />
+        <Tabs defaultActiveKey={2} id="uncontrolled-tab-example">
+          <Tab eventKey={1} title="Kid 1">
+            <CollapsableMapper data={this.props.dailyChores} path="dailyChore" field="chore" />
+            <CollapsableMapper data={this.props.weeklyChores} path="weeklyChore" field="chore" />
+          </Tab>
+          <Tab eventKey={2} title="Kid 2">
+            <CollapsableMapper data={this.props.dailyChores} path="dailyChore" field="chore" />
+            <CollapsableMapper data={this.props.weeklyChores} path="weeklyChore" field="chore" />
+          </Tab>
+          <Tab eventKey={3} title="Kid 3" disabled>
+            <CollapsableMapper data={this.props.dailyChores} path="dailyChore" field="chore" />
+            <CollapsableMapper data={this.props.weeklyChores} path="weeklyChore" field="chore" />
+          </Tab>
+        </Tabs>
+      </div>
+    );
+  }
 }
-export default DailyChores;
+export default Admin;
